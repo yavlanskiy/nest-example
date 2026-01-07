19d6f073919b58df62c157ac45b2ebdfc1c12486
Maksim Yavlyanskiy <max_yav@csltd.com.ua>
2026-01-08 00:46:28 +0200

    fix

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,3 +1,8 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsString } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsString({message: 'Title must be a string suka'})
   title: string;
 }
